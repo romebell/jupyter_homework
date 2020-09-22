6d46414bda3de708149db6a593382f2467e3af89
Chamon562 <Chamon562@gmail.com>
2020-09-22 09:06:42 -0700

    [update] Change wording for mult to multiply and div to divide to be more clear

--- calculator.py
+++ calculator.py
@@ -7,16 +7,16 @@
 # define a calculator function
 def calculator():
 #     make input to ask user what operation and number to solve
-    calc = input('What operation would you like to do? (add, sub, mult, div)\n')
+    calc = input('What operation would you like to do? (add, sub, multiply, divide)\n')
     num1 = input('Your first number\n')
     num2 = input('Your second number\n')
     if calc == 'add':
         result = int(num1) + int(num2)
     elif calc == 'sub':
         result = int(num1) - int(num2)
-    elif calc == 'mult':
+    elif calc == 'multiply':
         result = int(num1) * int(num2)
-    elif calc == 'div':
+    elif calc == 'divide':
         result = int(num1) / int(num2)
     return result
 print(calculator())
